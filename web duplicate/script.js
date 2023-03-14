1f45ef4ad5d4fd0e985561080e455fec179e862e
ShivrajPrachande <122004109+ShivrajPrachande@users.noreply.github.com>
2023-03-14 19:32:14 +0530

    Add files via upload

---
+++ web duplicate/script.js
@@ -0,0 +1,7 @@
+/************  typing animation  ****** */
+var typed = new Typed(".typing",{
+    strings:["","Programer","web Developer","Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
